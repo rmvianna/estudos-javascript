124c1484936705aaa19040c921030b79f2107425
rmvianna <marquez.vianna@gmail.com>
2024-08-13 08:38:03 -0300

    Explorando manipulacao DOM e formulario

--- code/js/paciente.js
+++ code/js/paciente.js
@@ -2,30 +2,38 @@ class Paciente {
 
     static #LIMITE_PESO = 500;
     static #LIMITE_ALTURA = 3;
+    static #LIMITE_GORDURA = 100;
 
-    constructor(peso, altura) {
-        this._peso = parseFloat(this.#validarPeso(peso));
-        this._altura = parseFloat(this.#validarAltura(altura));
+    constructor(nome, peso, altura, gordura) {
+        this._nome = nome;
+        this._peso = peso;
+        this._altura = altura;
+        this._gordura = gordura;
     }
 
+    get nome() {return this._nome;}
     get peso() {return this._peso;}
     get altura() {return this._altura;}
-    get imc() {return (this.peso / (this.altura * this.altura)).toFixed(2);}
-
-    #validarPeso(peso) {
-        if (!this.#ehNumeroValido(peso, Paciente.#LIMITE_PESO)) {
-            throw new Error("O peso '" + peso + "' não é válido");
+    get gordura() {return this._gordura;}
+    
+    get imc() {
+        if (this.ehPesoValido() && this.ehAlturaValida()) {
+            return (this.peso / (this.altura * this.altura)).toFixed(2);
         }
 
-        return peso;
+        throw new Error("Não é possível calcular IMC, pois peso e/ou altura é inválido");
     }
 
-    #validarAltura(altura) {
-        if (!this.#ehNumeroValido(altura, Paciente.#LIMITE_ALTURA)) {
-            throw new Error("A altura '" + altura + "' não é válida");
-        }
+    ehPesoValido() {
+        return this.#ehNumeroValido(this.peso, Paciente.#LIMITE_PESO);
+    }
 
-        return altura;
+    ehAlturaValida() {
+        return this.#ehNumeroValido(this.altura, Paciente.#LIMITE_ALTURA);
+    }
+
+    ehGorduraValida() {
+        return this.#ehNumeroValido(this.gordura, Paciente.#LIMITE_GORDURA);
     }
 
     #ehNumeroValido(numero, limiteSup) {
--- code/js/principal.js
+++ code/js/principal.js
@@ -1,19 +1,84 @@
+let PACIENTES = new Array();
+
 document.addEventListener("DOMContentLoaded", (event) => {
-    atualizarIMC();
+    carregarPacientes();
+    atualizarTabela();
+    
+    document.querySelector("#adicionar-paciente").addEventListener("click", (event) => {
+        event.preventDefault();
+
+        let formPaciente = document.querySelector("#form-adiciona");
+
+        let paciente = new Paciente(
+            formPaciente.nome.value,
+            formPaciente.peso.value,
+            formPaciente.altura.value,
+            formPaciente.gordura.value
+        );
+
+        PACIENTES.push(paciente);
+        adicionarPacienteTabela(paciente);
+    });
 });
 
-function atualizarIMC() {
+function carregarPacientes() {
     document.querySelectorAll("#tabela-pacientes .paciente").forEach((linha) => {
-        let imcTD = linha.querySelector(".info-imc");
-
-        try {
-            let pesoTd = linha.querySelector(".info-peso").textContent;
-            let alturaTd = linha.querySelector(".info-altura").textContent;
-
-            let paciente = new Paciente(pesoTd, alturaTd);
-            imcTD.textContent = paciente.imc;
-        } catch (e) {
-            imcTD.textContent = e.message;
-        }
+        PACIENTES.push(new Paciente(
+            linha.querySelector(".info-nome").textContent,
+            linha.querySelector(".info-peso").textContent,
+            linha.querySelector(".info-altura").textContent,
+            linha.querySelector(".info-gordura").textContent
+        ));
     });
+}
+
+function atualizarTabela() {
+    let tabelaPacientes = document.querySelector("#tabela-pacientes");
+
+    while (tabelaPacientes.firstChild) {
+        tabelaPacientes.removeChild(tabelaPacientes.lastChild);
+    }
+
+    PACIENTES.forEach(paciente => {
+        adicionarPacienteTabela(paciente);
+    });
+}
+
+function adicionarPacienteTabela(paciente) {
+    let tr = document.createElement("tr");
+    tr.classList.add("paciente");
+
+    let tdNome = document.createElement("td");
+    tdNome.classList.add("info-nome");
+    tdNome.textContent = paciente.nome;
+    tr.appendChild(tdNome);
+
+    let tdPeso = document.createElement("td");
+    tdPeso.classList.add("info-peso");
+    tdPeso.textContent = paciente.peso;
+    tr.appendChild(tdPeso);
+
+    let tdAltura = document.createElement("td");
+    tdAltura.classList.add("info-altura");
+    tdAltura.textContent = paciente.altura;
+    tr.appendChild(tdAltura);
+
+    let tdGordura = document.createElement("td");
+    tdGordura.classList.add("info-gordura");
+    tdGordura.textContent = paciente.gordura;
+    tr.appendChild(tdGordura);
+
+    let tdImc = document.createElement("td");
+    tdImc.classList.add("info-imc");
+
+    try {
+        tdImc.textContent = paciente.imc;
+    } catch (e) {
+        tdImc.textContent = e.message;
+        tr.classList.add("paciente-invalido");
+    }
+
+    tr.appendChild(tdImc);
+
+    document.querySelector("#tabela-pacientes").appendChild(tr);
 }
