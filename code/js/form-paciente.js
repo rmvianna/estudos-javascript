71238e8b5b1f907b6e0531d0af0d5f5d72475534
rmvianna <marquez.vianna@gmail.com>
2024-08-15 08:33:44 -0300

    Validando campos por Javascript

--- code/js/form-paciente.js
+++ code/js/form-paciente.js
@@ -15,7 +15,77 @@ function salvarPaciente() {
         formPaciente.gordura.value
     );
 
-    PacienteDAO.salvar(paciente);
+    if (validarPaciente(paciente)) {
+        PacienteDAO.salvar(paciente);
+        formPaciente.reset();
+    }
+}
 
-    formPaciente.reset();
+function validarPaciente(paciente) {
+    limparErros();
+    let tudoValido = true;
+
+    [validarNome, validarAltura, validarPeso, validarGordura].forEach(validacao => {
+        if (!validacao(paciente)) {
+            tudoValido = false;
+        }
+    });
+
+    return tudoValido;
+}
+
+function validarNome(paciente) {
+    let ehValido = paciente.ehNomeValido();
+    if (!ehValido) {
+        exibirMensagemErro("O nome do paciente é inválido", "nome");
+    }
+
+    return ehValido;
+}
+
+function validarAltura(paciente) {
+    let ehValido = paciente.ehAlturaValida();
+    if (!ehValido) {
+        exibirMensagemErro("A altura do paciente é inválida", "altura");
+    }
+
+    return ehValido;
+}
+
+function validarPeso(paciente) {
+    let ehValido = paciente.ehPesoValido();
+    if (!ehValido) {
+        exibirMensagemErro("O peso do paciente é inválido", "peso");
+    }
+
+    return ehValido;
+}
+
+function validarGordura(paciente) {
+    let ehValido = paciente.ehGorduraValida();
+    if (!ehValido) {
+        exibirMensagemErro("O percentual de gordura do paciente é inválido", "gordura");
+    }
+
+    return ehValido;
+}
+
+function exibirMensagemErro(msg, campo) {
+    let itemLista = document.createElement("li");
+    itemLista.textContent = msg;
+
+    document.querySelector("#mensagens-erro").appendChild(itemLista);
+    document.querySelector("#" + campo).classList.add("campo-invalido");
+}
+
+function limparErros() {
+    document.querySelectorAll(".campo").forEach(campo => {
+        campo.classList.remove("campo-invalido");
+    });
+
+    let listaErros = document.querySelector("#mensagens-erro");
+
+    while (listaErros.firstChild) {
+        listaErros.removeChild(listaErros.lastChild);
+    }
 }
